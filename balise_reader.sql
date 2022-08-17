67c342ff4bee6de860238a744fd674ddcb0aea60
Gansonre Ismael <proismaelgans2017@gmail.com>
2022-08-17 14:05:52 -0400

    Create balise_reader.sql

---
+++ balise_reader.sql
@@ -0,0 +1,52 @@
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('16689', 'ACTIF', 'NULL', '1', 'NULL', 'ABI.ANT-UWB60002', '2022-07-27 14:03:00.000', 'sur le chariot à outils de la MSE 1', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('33434', 'ACTIF', 'NULL', '2', 'NULL', 'ABI.ANT-UWB60007', '2022-07-27 14:07:00.000', 'sur le chariot à outils de la MSE 2', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('15562', 'ACTIF', 'NULL', '3', 'NULL', 'ABI.ANT-UWB60008', '2022-07-27 14:07:00.000', 'sur le chariot à outils de la MSE 3', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('1556562', 'INACTIF', 'NULL', '4', 'NULL', 'ABI.ANT-UWB60009', '2022-07-27 14:07:00.003', 'sur le chariot à outils de la MSE 4', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('15563', 'INACTIF', 'NULL', '5', 'NULL', 'ABI.ANT-UWB60010', '2022-07-27 14:07:00.003', 'sur le chariot à outils de la MSE 5', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('75656', 'INACTIF', 'NULL', '6', 'NULL', 'ABI.ANT-UWB60011', '2022-07-27 14:07:00.003', 'sur le chariot à outils de la MSE 6', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('345453', 'ACTIF', 'NULL', '7', 'NULL', 'ABI.ANT-UWB60012', '2022-07-27 14:07:00.007', 'sur le chariot à outils de la MSE 7', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('454534', 'ACTIF', 'NULL', '8', 'NULL', 'ABI.ANT-UWB60013', '2022-07-27 14:07:00.007', 'sur le chariot à outils de la MSE 8', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('4354', 'ACTIF', 'NULL', '9', 'NULL', 'ABI.ANT-UWB60014', '2022-07-27 14:07:00.007', 'sur le chariot à outils de la MSE 9', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('34534534', 'ACTIF', 'NULL', '10', 'NULL', 'ABI.ANT-UWB60015', '2022-07-27 14:07:00.007', 'sur le chariot à outils de la MSE 10', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('545765', 'ACTIF', 'NULL', '11', 'NULL', 'ABI.ANT-UWB60016', '2022-07-27 14:07:00.010', 'sur le chariot à outils de la MSE 11', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('436556', 'ACTIF', 'NULL', '12', 'NULL', 'ABI.ANT-UWB60017', '2022-07-27 14:07:00.010', 'sur le chariot à outils de la MSE 12', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('5677662', 'ACTIF', 'NULL', '13', 'NULL', 'ABI.ANT-UWB60018', '2022-07-27 14:07:00.010', 'sur le chariot à outils de la MSE 13', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('213262', 'ACTIF', 'NULL', '14', 'NULL', 'ABI.ANT-UWB60019', '2022-07-27 14:07:00.013', 'sur le chariot à outils de la MSE 14', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('13462', 'INACTIF', 'NULL', '15', 'NULL', 'ABI.ANT-UWB60020', '2022-07-27 14:07:00.013', 'sur le chariot à outils de la MSE 15', '', '');
+INSERT INTO [dbo].[noovelia_kencee_antenne] (Reader_uwb_id,Etat,Raison,ID,RFID_reader,Reader_name,Date_De_Mise_A_jour,Emplacement,,) VALUES ('23208', 'ACTIF', 'NULL', '16', 'NULL', 'ABI.ANT-UWB60078', '2022-07-27 14:03:00.000', 'sur le chariot à outils de la MSE 29');
+
+
+
+
+
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('51345', 'ABI.BAL-UWB60000', 'ACTIF', 'ABI.BAL-UWB60000', '', '2020-06-15 11:42:19.687', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2895', 'ABI.BAL-UWB60001', '', 'ABI.BAL-UWB60001', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('', 'ABI.BAL-UWB60002', '', 'ABI.BAL-UWB60002', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2896', 'ABI.BAL-UWB60003', '', 'ABI.BAL-UWB60003', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2892', 'ABI.BAL-UWB60004', '', 'ABI.BAL-UWB60004', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2891', 'ABI.BAL-UWB60005', '', 'ABI.BAL-UWB60005', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2431', 'ABI.BAL-UWB60006', '', 'ABI.BAL-UWB60006', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('73', 'ABI.BAL-UWB60007', '', 'ABI.BAL-UWB60007', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2853', 'ABI.BAL-UWB60008', '', 'ABI.BAL-UWB60008', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2894', 'ABI.BAL-UWB60009', '', 'ABI.BAL-UWB60009', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('82', 'ABI.BAL-UWB60010', '', 'ABI.BAL-UWB60010', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2300', 'ABI.BAL-UWB60011', '', 'ABI.BAL-UWB60011', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2850', 'ABI.BAL-UWB60012', '', 'ABI.BAL-UWB60012', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2428', 'ABI.BAL-UWB60013', '', 'ABI.BAL-UWB60013', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2893', 'ABI.BAL-UWB60014', '', 'ABI.BAL-UWB60014', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2435', 'ABI.BAL-UWB60015', '', 'ABI.BAL-UWB60015', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2890', 'ABI.BAL-UWB60016', '', 'ABI.BAL-UWB60016', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2436', 'ABI.BAL-UWB60017', '', 'ABI.BAL-UWB60017', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2851', 'ABI.BAL-UWB60018', '', 'ABI.BAL-UWB60018', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2854', 'ABI.BAL-UWB60019', '', 'ABI.BAL-UWB60019', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2433', 'ABI.BAL-UWB60020', '', 'ABI.BAL-UWB60020', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2438', 'ABI.BAL-UWB60021', '', 'ABI.BAL-UWB60021', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2898', 'ABI.BAL-UWB60022', '', 'ABI.BAL-UWB60022', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2439', 'ABI.BAL-UWB60023', '', 'ABI.BAL-UWB60023', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2899', 'ABI.BAL-UWB60024', '', 'ABI.BAL-UWB60024', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2301', 'ABI.BAL-UWB60025', '', 'ABI.BAL-UWB60025', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2425', 'ABI.BAL-UWB60026', '', 'ABI.BAL-UWB60026', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('5916', 'ABI.BAL-UWB60027', '', 'ABI.BAL-UWB60027', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2852', 'ABI.BAL-UWB60028', '', 'ABI.BAL-UWB60028', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2434', 'ABI.BAL-UWB60029', '', 'ABI.BAL-UWB60029', '', '', '', '');
+INSERT INTO [dbo].[noovelia_kencee_balise] (Tag_id,RFID_tag,Etat,Tag_name,Raison,Date_De_Mise_A_Jour,,) VALUES ('2427', 'ABI.BAL-UWB60030', '', 'ABI.BAL-UWB60030');
