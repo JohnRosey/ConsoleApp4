e5536747da05bbe1d750f83cb8cd91455a1aea3f
Gansonre Ismael <proismaelgans2017@gmail.com>
2022-08-18 15:07:13 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -65,19 +65,23 @@ namespace ConsoleApp4
                 SqlConnection SQLConnection = new SqlConnection();
                 SQLConnection.ConnectionString = @"Data Source = ABI-SMT-SQL-CL1.apm.alcoa.com; Database =SMART DFRM ;Integrated Security=SSPI";
 
-                string query = @" 
-           SELECT 
-D.[detection_id],
- D.[reader_uwb_id]
+                string query = @"SELECT  ( detection_id  ) ,
+      D.[reader_uwb_id]
       ,D.[tag_id]
-      ,D.[tag_temperature]
-      ,D.[distance]
-      ,D.[tag_motion]
+,D.tag_temperature
+      ,D.[distance],Emplacement
       ,D.[insert_timestamp]
-
-  FROM[ABI-MES-SQL-CL1.APM.ALCOA.COM].[RFID_SURAL].[dbo].[noovelia_kencee_detection] as D
-  WHERE D.distance BETWEEN 1.88 and 15.82 and insert_timestamp >'2021-12-31'
- ORDER BY detection_id Asc";
+	  
+	 
+      
+     
+  FROM [ABI-MES-QA.APM.ALCOA.COM].[RFID_SURAL_2].[dbo].[kencee_detection] as D
+INNER JOIN [ABI-MES-QA.APM.ALCOA.COM].[RFID_SURAL_2].dbo.noovelia_kencee_antenne as A
+ON A.Reader_uwb_id=D.reader_uwb_id 
+INNER JOIN [ABI-MES-QA.APM.ALCOA.COM].[RFID_SURAL_2].dbo.noovelia_kencee_balise as B
+ON B.Fonction='PINCE À CROUTE' and A.fonction='PINCE À CROUTE'
+and B.Nom_Emplacement=A.Nom_Emplacement 
+ORDER BY Emplacement ,insert_timestamp asc";
                 string query2 = @" 
           DECLARE @yesterday DATETIME
     = DATEADD(DAY, -1, CAST(GETDATE() AS DATE));
